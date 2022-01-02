018ca4f01e9ca265b4e935ae3817f0ace29a03e0
gutto-moura <gmourah1792@gmail.com>
2022-01-02 15:02:57 -0300

    Criando arquivo e iniciando o projeto

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//Janela de execução: 2019-11-10 09:00:00 até 2019-11-11 12:00:00
+
+var listJobs = [
+{
+"ID": 1,
+"Descricao": "Importação de arquivos de fundos",
+"Data Máxima de conclusão": "2019-11-10 12:00:00",
+"Tempo estimado": 2,
+},
+{
+"ID": 2,
+"Descricao": "Importação de dados da Base Legada",
+"Data Máxima de conclusão": "2019-11-11 12:00:00",
+"Tempo estimado": 4,
+},
+{
+"ID": 3,
+"Descricao": "Importação de dados de integração",
+"Data Máxima de conclusão": "2019-11-11 08:00:00",
+"Tempo estimado": 6,
+},
+];
+
+//VERIFICAR AS INFORMAÇÕES DO ARRAY
+console.log(listJobs[0]);
